77aedf1913518dd0f2fb5f8a2c783e054ed7b442
Bimalkeeth <bkaluarachchi@assetic.com>
2019-03-27 17:09:26 +1100

    lambda

--- Authorizer.go
+++ Authorizer.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"errors"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+)
+
 func main() {
+
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			// Verify 'aud' claim
+			aud := "YOUR_API_IDENTIFIER"
+			checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAud {
+				return token, errors.New("Invalid audience.")
+			}
+			// Verify 'iss' claim
+			iss := "https://dev-8xmkmv6f.au.auth0.com/"
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
+
+			return nil, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+	print(jwtMiddleware)
 
 }
